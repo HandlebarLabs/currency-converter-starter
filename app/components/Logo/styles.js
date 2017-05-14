ba55d612cd4709fe1dc4e3064aa1fb800c47b4f9
Spencer Carli <spencercarli@gmail.com>
2017-05-13 20:42:34 -0500

    Animated logo

--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,17 +4,22 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 const imageWidth = Dimensions.get('window').width / 2;
 
 export default EStyleSheet.create({
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
   container: {
     alignItems: 'center',
   },
   containerImage: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth,
+    width: '$largeContainerSize',
+    height: '$largeContainerSize',
   },
   logo: {
-    width: imageWidth / 2,
+    width: '$largeImageSize',
+    tintColor: '$primaryBlue',
   },
   text: {
     color: '$white',
