3c12a73a96d9dcbf09deed5144c685445c2cf88a
Spencer Carli <spencercarli@gmail.com>
2017-05-13 20:47:05 -0500

    Setup flatlist

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -12,4 +12,4 @@ EStyleSheet.build({
   $inputText: '#797979',
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
---
+++ app/screens/CurrencyList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { FlatList, Text, StatusBar, View } from 'react-native';
+
+import currencies from '../data/currencies';
+
+class CurrencyList extends Component {
+  handlePress = () => {
+    console.log('row press');
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar translucent={false} barStyle="default" />
+        <FlatList
+          data={currencies}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          keyExtractor={item => item}
+        />
+      </View>
+    );
+  }
+}
+
+export default CurrencyList;
