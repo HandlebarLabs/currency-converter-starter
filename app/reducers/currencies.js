076b2562a397d8105d72657dd14b799795c4dfae
Spencer Carli <spencercarli@gmail.com>
2017-06-06 09:50:44 -0500

    Making remote requests with saga

--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -3,59 +3,17 @@ import {
   SWAP_CURRENCY,
   CHANGE_BASE_CURRENCY,
   CHANGE_QUOTE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
 } from '../actions/currencies';
-
-// const initialState = {
-//   baseCurrency: 'USD',
-//   quoteCurrency: 'GBP',
-//   amount: 100,
-//   conversions: {},
-// };
 
 const initialState = {
   baseCurrency: 'USD',
   quoteCurrency: 'GBP',
   amount: 100,
-  conversions: {
-    USD: {
-      isFetching: false,
-      base: 'USD',
-      date: '2017-05-31',
-      rates: {
-        AUD: 1.3416,
-        BGN: 1.743,
-        BRL: 3.2515,
-        CAD: 1.3464,
-        CHF: 0.97104,
-        CNY: 6.813,
-        CZK: 23.547,
-        DKK: 6.6302,
-        GBP: 0.77858,
-        HKD: 7.7908,
-        HRK: 6.6068,
-        HUF: 273.77,
-        IDR: 13308,
-        ILS: 3.5431,
-        INR: 64.463,
-        JPY: 110.86,
-        KRW: 1118.4,
-        MXN: 18.765,
-        MYR: 4.281,
-        NOK: 8.4117,
-        NZD: 1.4071,
-        PHP: 49.77,
-        PLN: 3.7173,
-        RON: 4.0687,
-        RUB: 56.774,
-        SEK: 8.6942,
-        SGD: 1.3829,
-        THB: 34.07,
-        TRY: 3.5366,
-        ZAR: 13.133,
-        EUR: 0.89119,
-      },
-    },
-  },
+  conversions: {},
+  error: null,
 };
 
 const setConversions = (state, action) => {
@@ -97,6 +55,22 @@ export default (state = initialState, action) => {
         quoteCurrency: action.currency,
         conversions: setConversions(state, action),
       };
+    case GET_INITIAL_CONVERSION:
+      return { ...state, conversions: setConversions(state, { currency: state.baseCurrency }) };
+    case CONVERSION_RESULT:
+      return {
+        ...state,
+        baseCurrency: action.result.base,
+        conversions: {
+          ...state.conversions,
+          [action.result.base]: {
+            isFetching: false,
+            ...action.result,
+          },
+        },
+      };
+    case CONVERSION_ERROR:
+      return { ...state, error: action.error };
     default:
       return state;
   }
