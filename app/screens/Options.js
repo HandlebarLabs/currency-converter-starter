c1ef6c4ea7f80a6eeafe6008fa5ae8bb2f0c3811
Spencer Carli <spencercarli@gmail.com>
2017-08-22 17:22:13 -0500

    proptypes

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { ScrollView, StatusBar, Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { ListItem, Separator } from '../components/List';
