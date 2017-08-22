7f2b520953c8d5ac04d022c99c13a8e3446bf404
Spencer Carli <spencercarli@gmail.com>
2017-08-22 11:56:28 -0500

    upgrading packages

--- App.js
+++ App.js
@@ -10,10 +10,9 @@ const styles = StyleSheet.create({
   },
 });
 
-export default () => (
-  <View style={styles.container}>
+export default () =>
+  (<View style={styles.container}>
     <Text>Open up App.js to start working on your app!</Text>
     <Text>Changes you make will automatically reload.</Text>
     <Text>Shake your phone to open the developer menu.</Text>
-  </View>
-);
+  </View>);
