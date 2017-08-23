c9010e625f0ad3b0b54b96c057f100d2629bcf80
Spencer Carli <spencercarli@gmail.com>
2017-08-22 20:22:56 -0500

    Fix saga bug

--- app/config/sagas.js
+++ app/config/sagas.js
@@ -2,13 +2,15 @@ import { takeEvery } from 'redux-saga/effects';
 
 import { CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION, SWAP_CURRENCY } from '../actions/currencies';
 
-function* fetchLatestConversionRates(action) {
+const fetchLatestConversionRates = function* (action) {
   console.log('TODO: Update the things.', action);
   yield;
-}
+};
 
-export default function* rootSaga() {
+const rootSaga = function* () {
   yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates);
   yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates);
   yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates);
-}
+};
+
+export default rootSaga;
