081275c8b44dee8a688bc3edfa70dacbafc3e7d6
Spencer Carli <spencercarli@gmail.com>
2018-08-22 10:32:19 -0700

    Remove Fixer.io link

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
+import {
+  ScrollView, StatusBar, Platform, Linking,
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { ListItem, Separator } from '../components/List';
 
@@ -18,7 +20,7 @@ class Options extends Component {
   };
 
   handlePressSite = () => {
-    Linking.openURL('http://fixer.io').catch(() => alert('An error occured.'));
+    Linking.openURL('http://handlebarlabs.com').catch(() => alert('An error occured.'));
   };
 
   render() {
@@ -34,7 +36,7 @@ class Options extends Component {
         />
         <Separator />
         <ListItem
-          text="Fixer.io"
+          text="Handlebar Labs"
           onPress={this.handlePressSite}
           customIcon={<Ionicons name={`${ICON_PREFIX}-link`} size={ICON_SIZE} color={ICON_COLOR} />}
         />
