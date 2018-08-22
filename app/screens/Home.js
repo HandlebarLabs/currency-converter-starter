911591e7aeeb84cabbc6a01e291c4717cdf78f0f
Spencer Carli <spencercarli@gmail.com>
2018-08-22 10:52:29 -0700

    Fix linting issues

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StatusBar, KeyboardAvoidingView } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
